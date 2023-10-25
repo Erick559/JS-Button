461548426129a37efdb5042626ce5d1c8b4358c7
erick <erickungu82@gmail.com>
2023-10-25 11:29:08 +0300

    Update the JS code to add a new class unclicked to the button
    This class will track when the mouse is not clicked anymore to add a
    click animamtion

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const button = document.querySelector('button')
 
-button.addEventListener('click', function(){
+button.addEventListener('mousedown', function(){
     button.classList.add('clicked')
+})
+
+button.addEventListener('mouseup', function(){
+    button.classList.add('unclicked')
 })
